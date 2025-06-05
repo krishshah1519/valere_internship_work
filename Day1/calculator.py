942256746ff45487aca12940d051adac4fe52322
krishshah1519 <krishshah1519@gmail.com>
2025-06-05 12:28:33 +0530

    Applied exception handling and logging

--- Day1/calculator.py
+++ Day1/calculator.py
@@ -27,44 +27,56 @@ def multiply(x, y):
 
 def divide(x, y):
     logging.info("Successfully!! Entered Divide Function")
-    if y == 0:
-        logging.warning("Invalid input resulting in error")
-        raise Exception("Error! Cannot divide by zero")
-    else:
+    try:
         print("Result: ", x/y)
         logging.info(f"The Remainder of {x} and {y} is: {x / y}")
+    except ZeroDivisionError:
+        print("Cannot Divide a digit with 0")
+        logging.warning("The user tried to divided a number by Zero ")
 
 
-logging.info("Program Starts")
-print("Please Select a Operation")
+while True:
+    logging.info("Program Starts")
+    print("Please Select a Operation")
+
+    inputOperator = int(input("1).Add \n2).Subtract \n3).Multiply \n4).Divide "
+                              "\n5).Exit Program \n"))
+    logging.info("Operator Selected Succesfully")
+
+    try:
+        if inputOperator > 5 or inputOperator == 0:
+            logging.error("Error raised due to invalid Operation Selection")
+            raise Exception("Invalid Operation Selected")
+        
+        if inputOperator == 5:
+            logging.info("User Wants to Quit the Program")
+            print("The Program is Ending")
+            break
+
+        else:
+            inputNum1 = int(input("Enter First Number: "))
+            logging.info("Input Number one taken Successfully")
+            inputNum2 = int(input("Enter Second Number: "))
+            logging.info("Input Number second taken Successfully")
+            logging.info(f"The Number one is: {inputNum1} ,"
+                         f" The Number two is: {inputNum2}")
+
+    except Exception as E:
+        print("Invalid Operation selected")
 
 
-inputOperator = int(input("1).Add \n2).Subtract \n3).Multiply \n4).Divide\n"))
-logging.info("Operator Selected Succesfully")
+    if inputOperator == 1:
+        logging.info("Addition is Chosen")
+        add(inputNum1, inputNum2)
 
-if inputOperator > 4 or inputOperator == 0:
-    logging.warning("Error raised due to invalid Operation Selection")
-    raise Exception("Invalid Operation Selected")
+    if inputOperator == 2:
+        logging.info("Subtraction is Chosen")
+        subtract(inputNum1, inputNum2)
 
-inputNum1 = int(input("Enter First Number: "))
-logging.info("Input Number one taken Successfully")
-inputNum2 = int(input("Enter Second Number: "))
-logging.info("Input Number second taken Successfully")
-logging.info(f"The Number one is: {inputNum1} ,"
-             f" The Number two is: {inputNum2}")
+    if inputOperator == 3:
+        logging.info("Multiplication is Chosen")
+        multiply(inputNum1, inputNum2)
 
-if inputOperator == 1:
-    logging.info("Addition is Chosen")
-    add(inputNum1, inputNum2)
-
-if inputOperator == 2:
-    logging.info("Subtraction is Chosen")
-    subtract(inputNum1, inputNum2)
-
-if inputOperator == 3:
-    logging.info("Multiplication is Chosen")
-    multiply(inputNum1, inputNum2)
-
-if inputOperator == 4:
-    logging.info("Divison is Chosen")
-    divide(inputNum1, inputNum2)
+    if inputOperator == 4:
+        logging.info("Divison is Chosen")
+        divide(inputNum1, inputNum2)
